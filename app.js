f96ee30e6fceeb300005024f24d3b470f7332347
kuldeep <kuldeepsingh.singh77@gmail.com>
2026-01-22 20:46:50 +0530

    updateing name

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("CI/CD Node App Running 🚀");
+    res.send("CI/CD Node App Running 🚀 Awdesh");
 });
 
 app.listen(3005, () => {
